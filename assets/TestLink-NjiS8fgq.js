d7936d6169c7bada915ef478f577f951b222d779
dogedefi <93388896+dogedefi@users.noreply.github.com>
2024-10-14 12:35:38 +0000

    Deploying to gh-pages from @ openad-protocol/openad-protocol.github.io@19b39f8b768b89ec39ba3cbcc43e6767ae787706 🚀

---
+++ assets/TestLink-NjiS8fgq.js
@@ -0,0 +1,1 @@
+import{t as n,j as e,v as t,w as i,n as a}from"./index-BZQymVFI.js";const l=()=>{const{classes:s}=n();return e.jsxs("div",{className:s.root,children:[e.jsx(t,{className:s.menuLink,title:"Testing only",to:i.dev.generatePath(),children:"🧪 Test menu"}),a.offlineTesting&&e.jsx("span",{children:"⚠️ Offline testing is enabled"})]})};export{l as TestLink};
